8f6d231b06bdd500e2f4660e309eb32c31ec560e
Nguyen Duy Anh Quan <NguyenDuyAnhQuan@student.tudelft.nl>
2022-03-05 16:48:06 +0100

    remove more imports

--- commons/src/main/java/commons/Game.java
+++ commons/src/main/java/commons/Game.java
@@ -5,7 +5,6 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import javax.persistence.*;
-import java.util.List;
 
 import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
 
