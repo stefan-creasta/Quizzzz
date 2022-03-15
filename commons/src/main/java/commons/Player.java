b503598ed888b9d6128fbb10721660e598e3366b
Mert Bora İnevi <M.B.Inevi@student.tudelft.nl>
2022-03-15 03:28:16 +0100

    add commons changes for server-session-managements

---
+++ commons/src/main/java/commons/Player.java
@@ -0,0 +1,21 @@
+package commons;
+
+public class Player {
+    private static long idGenerator = 0L;
+
+    public long id;
+    public String username;
+    public long score;
+    public long gameId;
+    public Timer timer;
+    public String answer;
+
+    public Player(String username, long score) {
+        this.id = idGenerator++;
+        this.username = username;
+        this.score = score;
+        this.timer = new Timer(0, 30);
+        this.answer = null;
+        this.gameId = -1;
+    }
+}
