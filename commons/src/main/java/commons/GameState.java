f041589ac2a614257f76b6ee98147e11110332df
Mert Bora İnevi <M.B.Inevi@student.tudelft.nl>
2022-03-15 16:39:30 +0100

    add commons changes after the timer unmarshaling solution

--- commons/src/main/java/commons/GameState.java
+++ commons/src/main/java/commons/GameState.java
@@ -1,32 +1,29 @@
 package commons;
 
-import java.time.Duration;
-
 public class GameState {
     public enum Stage {QUESTION, INTERVAL};
-
     //roughly stores which screen should be displayed during the game session.
     public Stage stage;
     public Question question;
-    private Player player;
     //set to true if only the playerId needs to be processed.
     public boolean isError;
     public String message;
     public long timerSyncLong;
-    public Duration duration;
+    public long duration;
+    public long playerId;
+    public String username;
+    public String playerAnswer;
 
     public GameState(Question question, Player player) {
-        this.player = player;
         this.question = question;
         if (player != null) setPlayer(player);
     }
 
     public void setPlayer(Player player) {
         this.timerSyncLong = player.timer.getSynchronizationLong();
-        this.duration = player.timer.getDuration();
-    }
-
-    public Player getPlayer() {
-        return player;
+        this.duration = player.timer.getDurationLong();
+        this.playerId = player.id;
+        this.username = player.username;
+        this.playerAnswer = player.answer;
     }
 }
